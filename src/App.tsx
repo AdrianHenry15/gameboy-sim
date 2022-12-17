ae8c62b57dc68cb1bf88ffe19f4b146965232593
Adrian <adrianhenry2115@gmail.com>
2022-12-17 12:58:40 -0500

    navbar edit

--- src/App.tsx
+++ src/App.tsx
@@ -107,6 +107,7 @@ const App = () => {
 		<div className='App'>
 			{enter && (
 				<div className='screen-container'>
+					<NavBar />
 					<div className='device-container'>{renderConsole()}</div>
 				</div>
 			)}
